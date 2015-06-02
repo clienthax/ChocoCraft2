ba36025b3699d8e6eaf61a9d7a95aa40bb4b2caf
Clienthax <clienthax@gmail.com>
2015-06-02 19:20:42 +0100

    Todo list.
    Signed-off-by: Clienthax <clienthax@gmail.com>

--- src/main/java/uk/co/haxyshideout/chococraft2/ChocoCraft2.java
+++ src/main/java/uk/co/haxyshideout/chococraft2/ChocoCraft2.java
@@ -19,6 +19,28 @@ import uk.co.haxyshideout.chococraft2.proxys.ServerProxy;
  */
 @Mod(modid = Constants.MODID, name = Constants.MODNAME, version = Constants.MODVERSION)
 public class ChocoCraft2 {
+	/*
+	TODO list
+	chicibos + growing + growing by cake
+	chocobo bag code
+	special ability code (possibly a extra handler for this), flying , falling damage, fire proof, speed etc
+	implement chocobo whistle, should tp the last chocobo you rode to you aslong as you are in the same world
+	breeding!
+	the special gyshall recipies (red etc)
+	achievements
+	chocobo gui needs to open on the client from the server, funtimes..?
+	worldgen for ghyhals,
+	need to make yellow chocobos spawn in world
+	purple egg needs to spawn the bloody chocobo
+	chocobos must stare at people. is best
+	whole config system - use configurate, is awesome. as forges config system sucks.
+	chocopedia text - needs to be translatable
+
+	additions
+	dyable collars! - going to need to get that bloody image generator working for this stuff..
+	whistle, makes last ridden chocobo return to you
+
+	 */
 
 	@SidedProxy(clientSide = "uk.co.haxyshideout.chococraft2.proxys.ClientProxy", serverSide = "uk.co.haxyshideout.chococraft2.proxys.ServerProxy")
 	public static ServerProxy proxy;
