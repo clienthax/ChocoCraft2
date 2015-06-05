15a7cac821a6a413cb1e470d9cbb97b16ac5cd7b
Clienthax <clienthax@gmail.com>
2015-06-05 03:26:10 +0100

    Chocopedia book now works + fully translatable

--- src/main/java/uk/co/haxyshideout/chococraft2/events/EventHandler.java
+++ src/main/java/uk/co/haxyshideout/chococraft2/events/EventHandler.java
@@ -1,15 +1,35 @@
 package uk.co.haxyshideout.chococraft2.events;
 
+import com.google.common.io.CharSource;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreenBook;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
+import net.minecraft.nbt.NBTTagString;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.entity.player.BonemealEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.eventhandler.Event;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import ninja.leaping.configurate.ConfigurationNode;
+import ninja.leaping.configurate.hocon.HoconConfigurationLoader;
+import org.apache.commons.io.IOUtils;
 import uk.co.haxyshideout.chococraft2.blocks.GysahlStemBlock;
 import uk.co.haxyshideout.chococraft2.config.Additions;
+import uk.co.haxyshideout.chococraft2.config.Constants;
 import uk.co.haxyshideout.chococraft2.spawner.ChocoboSpawner;
+import uk.co.haxyshideout.haxylib.utils.FormattingHelper;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 /**
@@ -18,6 +38,47 @@ import java.util.List;
 public class EventHandler {
 
 	int spawnTimer = 0;
+
+	@SubscribeEvent
+	@SideOnly(Side.CLIENT)
+	public void onRightClickChocopedia(PlayerInteractEvent event) throws IOException {//TODO this is a terrible way to do this
+		if(event.action == PlayerInteractEvent.Action.RIGHT_CLICK_AIR) {
+			if(event.entityPlayer.getHeldItem() != null && event.entityPlayer.getHeldItem().getItem() == Additions.chocopediaItem) {
+				ItemStack book = new ItemStack(Items.written_book);
+				NBTTagCompound tagCompound = new NBTTagCompound();
+				tagCompound.setString("title", "Chocopedia");
+				tagCompound.setString("author", "Clienthax");
+				NBTTagList pageList = new NBTTagList();
+
+				String languageCode = Minecraft.getMinecraft().gameSettings.language;
+
+				InputStream inputStream;
+				try {
+					inputStream = Minecraft.getMinecraft().getResourceManager().getResource(new ResourceLocation(Constants.MODID, "lang/chocopedia/" + languageCode +".hocon")).getInputStream();
+				} catch (FileNotFoundException e) {
+					inputStream = Minecraft.getMinecraft().getResourceManager().getResource(new ResourceLocation(Constants.MODID, "lang/chocopedia/en_US.hocon")).getInputStream();
+				}
+
+				byte[] data = new byte[inputStream.available()];
+				IOUtils.readFully(inputStream, data);
+				ConfigurationNode configurationLoader = HoconConfigurationLoader.builder().setSource(CharSource.wrap(new String(data))).build().load();
+
+				List<? extends ConfigurationNode> pages = configurationLoader.getNode("pages").getChildrenList();
+				for(ConfigurationNode node : pages) {
+					String pageText = "";
+					List<? extends ConfigurationNode> lines = node.getChildrenList();
+					for(ConfigurationNode line : lines)
+						pageText += line.getString();
+					pageList.appendTag(new NBTTagString(FormattingHelper.convertFormattingCodes(pageText)));
+				}
+				tagCompound.setTag("pages", pageList);
+				book.setTagCompound(tagCompound);
+
+				Minecraft.getMinecraft().displayGuiScreen(new GuiScreenBook(event.entityPlayer, book, false));
+				event.setCanceled(true);
+			}
+		}
+	}
 
 	@SubscribeEvent
 	public void onBoneMealUse(BonemealEvent event) {
---
+++ src/main/java/uk/co/haxyshideout/haxylib/utils/FormattingHelper.java
@@ -0,0 +1,43 @@
+package uk.co.haxyshideout.haxylib.utils;
+
+import net.minecraft.util.EnumChatFormatting;
+
+import java.util.regex.Pattern;
+
+/**
+ * Created by clienthax on 5/6/2015.
+ */
+public class FormattingHelper {
+
+	/**
+	 * Converts the formatting oodes from &x format to the internal format used by mc.
+	 * @param readable the string to swap the control codes for
+	 */
+	public static String convertFormattingCodes(String readable) {
+		readable = readable.replaceAll(Pattern.quote("&l"), EnumChatFormatting.BOLD+"");
+		readable = readable.replaceAll(Pattern.quote("&n"), EnumChatFormatting.UNDERLINE+"");
+		readable = readable.replaceAll(Pattern.quote("&o"), EnumChatFormatting.ITALIC+"");
+		readable = readable.replaceAll(Pattern.quote("&m"), EnumChatFormatting.STRIKETHROUGH+"");
+		readable = readable.replaceAll(Pattern.quote("&r"), EnumChatFormatting.RESET+"");
+
+		readable = readable.replaceAll(Pattern.quote("&0"), EnumChatFormatting.BLACK+"");
+		readable = readable.replaceAll(Pattern.quote("&1"), EnumChatFormatting.DARK_BLUE+"");
+		readable = readable.replaceAll(Pattern.quote("&2"), EnumChatFormatting.DARK_GREEN+"");
+		readable = readable.replaceAll(Pattern.quote("&3"), EnumChatFormatting.DARK_AQUA+"");
+		readable = readable.replaceAll(Pattern.quote("&4"), EnumChatFormatting.DARK_RED+"");
+		readable = readable.replaceAll(Pattern.quote("&5"), EnumChatFormatting.DARK_PURPLE+"");
+		readable = readable.replaceAll(Pattern.quote("&6"), EnumChatFormatting.GOLD+"");
+		readable = readable.replaceAll(Pattern.quote("&7"), EnumChatFormatting.GRAY+"");
+		readable = readable.replaceAll(Pattern.quote("&8"), EnumChatFormatting.DARK_GRAY+"");
+		readable = readable.replaceAll(Pattern.quote("&9"), EnumChatFormatting.BLUE+"");
+		readable = readable.replaceAll(Pattern.quote("&a"), EnumChatFormatting.GREEN+"");
+		readable = readable.replaceAll(Pattern.quote("&b"), EnumChatFormatting.AQUA+"");
+		readable = readable.replaceAll(Pattern.quote("&c"), EnumChatFormatting.RED+"");
+		readable = readable.replaceAll(Pattern.quote("&d"), EnumChatFormatting.LIGHT_PURPLE+"");
+		readable = readable.replaceAll(Pattern.quote("&e"), EnumChatFormatting.YELLOW+"");
+		readable = readable.replaceAll(Pattern.quote("&f"), EnumChatFormatting.WHITE+"");
+
+
+		return readable;
+	}
+}
